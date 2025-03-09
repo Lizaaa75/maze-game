007962de00b333dfad83306505aeced714c8f9a8
Lizaaa75 <lizakot862@gmail.com>
2025-03-09 15:55:40 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -52,23 +52,50 @@ class Enemy(GameSprite):
         if self.direction == 'right':
             self.rect.x += self.speed
 
+class Wall(sprite.Sprite):
+    def __init__(self, color1, color2, color3, wall_x, wall_y, wall_w, wall_h):
+        super().__init__()
+        self.color1 = color1
+        self.color2 = color2
+        self.color3 = color3 
+        self.width = wall_w
+        self.height = wall_h
+        self.image = Surface((self.width, self.height))
+        self.image.fill((color1, color2, color3))
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+        self.rect.y = wall_y
 
 
+    def draw_wall(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+wall1 = Wall(86, 2, 31, 60, 60, 20, 350)
+wall2 = Wall(86, 2, 31, 60, 60, 550, 20)
+wall3 = Wall(86, 2, 31, 600, 60, 20, 350)
+wall4 = Wall(86, 2, 31, 170, 150, 20, 350)
+wall5 = Wall(86, 2, 31, 170, 150, 140, 20)
+wall6 = Wall(86, 2, 31, 300, 150, 20, 100)
+wall7 = Wall(86, 2, 31, 300, 250, 120, 20)
+wall8 = Wall(86, 2, 31, 400, 150, 20, 100)
+wall9 = Wall(86, 2, 31, 400, 150, 120, 20)
+wall10 = Wall(86, 2, 31, 500, 150, 20, 260)
 
 win_height = 500
 win_width = 700
 
 window = display.set_mode((win_width, win_height))
 
-background = transform.scale(image.load("background.jpg"), (win_width, win_height))
+background = transform.scale(image.load("trees.jpg"), (win_width, win_height))
 
 display.set_caption("Лабіринт")
 
 
-
-player = Player('hero.png', 10, win_height-80, 4)
-enemy = Enemy('cyborg.png', win_width-80, 80, 2)
-final = GameSprite('treasure.png', win_width-80, win_height-80, 0)
+player = Player('hedgehog.png', 10, win_height-80, 4)
+enemy = Enemy('brick.png', win_width-80, 80, 2)
+final = GameSprite('milk.png', win_width-80, win_height-80, 0)
 
 
 game = True
@@ -78,14 +105,32 @@ FPS = 60
 mixer.init()
 mixer.music.load('jungles.ogg')
 mixer.music.play()
+kick = mixer.Sound('kick.ogg')
+money = mixer.Sound('money.ogg')
 
 while game:
     window.blit(background, (0, 0))
 
 
-    player.update()
+    player.update() 
+    player.left()
+    player.right()
+    player.down()
+
 
     enemy.update()
+
+    wall1.draw_wall()
+    wall2.draw_wall()
+    wall3.draw_wall()
+    wall4.draw_wall()
+    wall5.draw_wall()
+    wall6.draw_wall()
+    wall7.draw_wall()
+    wall8.draw_wall()
+    wall9.draw_wall()
+    wall10.draw_wall()
+
 
     player.draw()
     enemy.draw()
@@ -94,6 +139,25 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    if sprite.collide_rect(player, wall1) or \
+    sprite.collide_rect(player, wall2) or \
+    sprite.collide_rect(player, wall3) or \
+    sprite.collide_rect(player, wall4) or \
+    sprite.collide_rect(player, wall5) or \
+    sprite.collide_rect(player, wall6) or \
+    sprite.collide_rect(player, wall7) or \
+    sprite.collide_rect(player, wall8) or \
+    sprite.collide_rect(player, wall9) or \
+    sprite.collide_rect(player, wall10) or \
+    sprite.collide_rect(player, enemy):
+        kick.play()
+        player.rect.x = 100
+        player.rect.y = 400
+    if sprite.collide_rect(player, final):
+        time.delay(1000)
+        money.play()
+        game = False
 
     
     display.update()
